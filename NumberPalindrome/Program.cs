44b2b4d62fce0158ebd2332f3ecf79b9e3a3e664
eyrealex <eyrealex97@gmail.com>
2024-06-27 00:25:51 +0100

    adding solutions for integer and string palindromes

---
+++ NumberPalindrome/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace NumberPalindrome
+{
+    public class Program
+    {
+
+        public static bool Palindrome(int X) {
+
+            int reversed = 0; //init reverse to 0
+            int original = X;
+
+            if(X < 0) // check for negative numbers
+            {
+                return false;
+            }
+
+            while(X != 0) // keep looping while X is not equal to 0
+            {
+                int remainder = X % 10; // get the very last digit of the number using a modulus
+                reversed = reversed * 10 + remainder; // start adding this number to the reversed varaible without overriting the variable
+                X /= 10; // remove last digit from X to reset it for the next iteration
+            }
+
+            return original == reversed; //return true if both numbers are equal meaning a palindrome was found
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            int num = 101;
+            Console.WriteLine(Palindrome(num));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringPalindrome/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Text.RegularExpressions;
+
+namespace StringPalindrome
+{
+    public class Program
+    {
+
+        public static string Palindrome(string str)
+        {
+
+            str = str.ToLower(); //convert the string to all lower case
+            str = Regex.Replace(str, "[^a-z]", ""); //remove all characters that are not lowercase a-z, replaces anything else with a blank string
+
+            int valLength = str.Length;
+            int end = valLength - 1;
+            int counter = 0;
+
+            for (int i = 0; i < valLength / 2; i++) //loop through half of the array
+            {
+                if (str[i] != str[valLength - i - 1]) //if value at the start does not match the value at the end
+                {
+                    return "Not Palindrome"; //no palindrome found
+                }
+            }
+
+            return "Palindrome"; //otherwise it worked
+        
+
+
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            string str = "Han!nah";
+            Console.WriteLine(Palindrome(str));
+        }
+    }
+}
